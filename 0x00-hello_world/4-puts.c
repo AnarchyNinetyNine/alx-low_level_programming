bde8f67461f662f4c496a0b832a0ad90b21e03b2
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-16 17:18:25 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-#include <stidio.h>
+#include <stdio.h>
 /*
  * main - Print a line of code with puts
  * Return: 0 (Sussess)
- * */
+ */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
