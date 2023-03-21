b78a0c10d5e67f71ddb96fd001eacf950935e73c
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-20 19:07:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that Prints numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+   for (i=0; i< 10;i++)
+   {
+	printf("%d \n", i);
+   }
+    return (0);
+}
