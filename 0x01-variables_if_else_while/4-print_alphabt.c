d24a2d5911aea6d4ba936b43bf97c3bc8a8ecd7b
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-20 19:35:17 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that Prints alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        char c[24] = "abcdfghijklmnoprstuvwxyz";
+
+        for (i = 0; i < 24; i++)
+        {
+                putchar(c[i]);
+        }
+        printf("\n");
+        return (0);
+}
