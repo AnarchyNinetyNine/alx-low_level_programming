9b004ec2eee815af3c7153e60d691b7cf0434523
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-22 19:23:34 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,17 @@ int print_sign(int c)
 {
 	if (c > 0)
 	{
+		_putchar(43);
 		return (1);
 	}
 	else if (c < 0)
 	{
+		_putchar(45);
 		return (0);
 	}
 	else
 	{
+		_putchar(48);
 		return (0);
 	}
 	_putchar('\n');
