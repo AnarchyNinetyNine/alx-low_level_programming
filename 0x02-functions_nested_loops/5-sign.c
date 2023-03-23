da6f02bc9f74ff958127fb4da4ab2fb3e5fe7358
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-22 19:25:53 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,17 +2,17 @@
 /**
  * print_sign - show the sign of a number
  * Description: 'prints the sign of a numebr'
- * @c: the caracter in ASCII code
+ * @n: the input n is an integer
  * Return: 1 for positive numbers, -1 for negative ones, 0 else
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
 	}
-	else if (c < 0)
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (0);
