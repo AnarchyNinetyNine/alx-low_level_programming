c62597c78ddf271ed51586d6dd05cfa732802821
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-22 19:05:43 -0700

    islower

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * _isalphz - show 1 if c is a letter
+ * _isalpha - show 1 if c is a letter
  * Description: 'prints _putchar stdout'
  * @c: the caracter in ASCII code
  * Return: 1 for lowercase or uppercase letters 0 else
  */
 int _isalpha(int c)
 {
-        if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
-        _putchar('\n');
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
