4b0858bc4ed056e651552303a3b8a118581f08c6
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-22 19:46:29 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - show the sum of two integers
+ * Description: 'prints the sum of two variables'
+ * @i: the input i is an integer
+ * @j: the input j is an integer
+ * Return: the sum of two integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
