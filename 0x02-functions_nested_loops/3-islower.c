64989569a1ecab94337565297ddfac6da01ee445
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-22 18:48:11 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - show 1 if c is lowercase
+ * Description: 'prints _putchar stdout'
+ * Return: 1 for lower 0 else
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
