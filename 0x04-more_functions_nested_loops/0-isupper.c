6cfa45a00a17e653fbafbbb047ddb6bbedce1d46
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-03-24 14:35:30 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ * Description: check upper
+ * @c: A Value from ASCII table
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 97 && <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
