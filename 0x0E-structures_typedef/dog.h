9f5369e02f28ced9df2b3dee4dd2117428ccb9e8
AnarchyNinetyNine <franckenstein999@gmail.com>
2023-04-18 16:42:15 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - structure definition
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+	}
+	return (0);
+}
