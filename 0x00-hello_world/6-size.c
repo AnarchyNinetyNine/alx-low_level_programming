80ab8271227bf2294c54ab2126ba8553d896f956
Idris <franckenstein999@gmail.com>
2023-12-14 15:50:19 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * printf - display formatted output to standard output
+ * return 0
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long int) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long int) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long int) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long int) sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long int) sizeof(float));
+	return (0);
+}
