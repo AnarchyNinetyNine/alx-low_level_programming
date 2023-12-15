576080d013fb5b30d166137830559b8715d177ca
Idris <franckenstein999@gmail.com>
2023-12-14 17:16:57 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,9 @@
 
 int main(void)
 {
-	const char *errorMessage = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *errorMessage_chunk_1 = "and that piece of art is useful\"";
+	const char *errorMessage_chunck_2 = " - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, "%s", errorMessage);
+	fprintf(stderr, "%s %s", errorMessage_chunk_1, errorMessage_chunck_2);
 	return (1);
 }
