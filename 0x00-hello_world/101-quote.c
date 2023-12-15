c380ce5fe3a8b68359ed66434713aeae71888fea
Idris <franckenstein999@gmail.com>
2023-12-14 17:18:38 -0800

    101-quote.c Hot fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,6 +11,6 @@ int main(void)
 	const char *errorMessage_chunk_1 = "and that piece of art is useful\"";
 	const char *errorMessage_chunck_2 = " - Dora Korpar, 2015-10-19\n";
 
-	fprintf(stderr, "%s %s", errorMessage_chunk_1, errorMessage_chunck_2);
+	fprintf(stderr, "%s%s", errorMessage_chunk_1, errorMessage_chunck_2);
 	return (1);
 }
