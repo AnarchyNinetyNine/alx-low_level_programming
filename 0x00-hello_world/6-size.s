685339f55b0b716af13379e8bd94ce1f8ec0e5c3
Idris <franckenstein999@gmail.com>
2023-12-15 16:32:41 -0800

    Hot-fix

---
+++ 0x00-hello_world/6-size.s
@@ -0,0 +1,83 @@
+	.file	"6-size.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Size of a char: %lu byte(s)\n"
+.LC1:
+	.string	"Size of an int: %lu byte(s)\n"
+	.align 8
+.LC2:
+	.string	"Size of a long int: %lu byte(s)\n"
+	.align 8
+.LC3:
+	.string	"Size of a long long int: %lu byte(s)\n"
+.LC4:
+	.string	"Size of a float: %lu byte(s)\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	$1, -40(%rbp)
+	movq	$4, -32(%rbp)
+	movq	$8, -24(%rbp)
+	movq	$8, -16(%rbp)
+	movq	$4, -8(%rbp)
+	movq	-40(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-32(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-24(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movq	-8(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC4(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
