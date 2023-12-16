685dd413e8070d7f153198089afa5418068ff21e
Idris <franckenstein999@gmail.com>
2023-12-15 18:21:04 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Starting point
+ * putchar - Print out a character to stdout
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+
+	}
+	putchar('\n');
+	return (0);
+}
