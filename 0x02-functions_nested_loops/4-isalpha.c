956ba952cb71adcbdced947864a5f2ae5bfc9c86
Idris <franckenstein999@gmail.com>
2023-12-19 17:38:21 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for uppercase characters
+ * @c: character type argument for _islower()
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
