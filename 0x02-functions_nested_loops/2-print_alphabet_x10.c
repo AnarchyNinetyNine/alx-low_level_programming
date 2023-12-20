2020716eb61675a7751f28a0708ca21f2d0bbfa5
Idris <franckenstein999@gmail.com>
2023-12-19 17:04:00 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints x10 alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	c = 'a';
+	i = 1;
+	while (i <= 10)
+		while (c <= 'z')
+			_putchar(c);
+		_putchar('\n');
+}
