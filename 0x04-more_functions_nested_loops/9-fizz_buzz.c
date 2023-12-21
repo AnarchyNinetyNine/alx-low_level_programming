4a22a19c9c17f802b4034d2f576df5eb8b2a8e1c
Idris <franckenstein999@gmail.com>
2023-12-21 15:57:33 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,68 +2,35 @@
 
 /**
  * main - FizzBuzz interview.
- * isCommon - Checks for multiples of both three and five.
- * isMulThree - Checks for multiples of three.
- * isMulThree - Checks for multiples of five.
  * Return: Always 0 (Success).
  */
 
-int isMulThree(int n)
-{
-	if ((n % 3) == 0)
-		return (1);
-	else
-		return (0);
-}
-
-int isMulFive(int n)
-{
-	if ((n % 5) == 0)
-		return (1);
-	else
-		return (0);
-}
-int isCommon(int n)
-{
-	if ((n % 3) == 0 && (n % 5) == 0)
-		return (1);
-	else
-		return (0);
-}
 int main(void)
 {
 	char fizz[] = "Fizz";
 	char buzz[] = "Buzz";
 	char fizzBuzz[] = "FizzBuzz";
-	int number;
+	int number, lastNumber;
 
 	number = 1;
-	while (number <= 100)
+	lastNumber = 100;
+	while (number <= lastNumber)
 	{
-		if (isCommon(number))
-		{
+		if (number % 3 == 0 && number % 5 == 0)
 			printf("%s", fizzBuzz);
-			printf(" ");
-			++number;
-		}
-		else if (isMulThree(number))
-		{
+		else if (number % 3 == 0)
 			printf("%s", fizz);
-			printf(" ");
-			++number;
-		}
-		else if (isMulFive(number))
-		{
+		else if (number % 5 == 0)
 			printf("%s", buzz);
-			printf(" ");
-			++number;
-		}
 		else
-		{
-			printf("%d", number++);
+			printf("%d", number);
+		
+		if (number != lastNumber)
 			printf(" ");
-		}
+		else
+			printf("\n");
+
+		++number;
 	}
-	printf("\n");
 	return (0);
 }
