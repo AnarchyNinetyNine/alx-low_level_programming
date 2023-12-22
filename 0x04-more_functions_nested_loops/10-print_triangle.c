646746dae1110320a3a57e78691edf2e1ec29bc6
Idris <franckenstein999@gmail.com>
2023-12-22 12:51:29 -0800

    Hot fix

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, followed by a new line.
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	char charPrinted;
+	char endLine;
+	int i, j;
+
+	charPrinted = '#';
+	endLine = '\n';
+	for (i = 1; i <= size; ++i)
+	{
+		for (j = 1; j <= size; ++j)
+		{
+			if ((i + j) <= size)
+				_putchar(' ');
+			else
+				_putchar(charPrinted);
+		}
+		_putchar(endLine);
+	}
+}
