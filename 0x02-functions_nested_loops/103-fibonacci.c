e5fe8b95c7e09c612f63972670d9a05f9298ed43
Idris <franckenstein999@gmail.com>
2023-12-25 17:47:05 -0800

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define THRESHOLD 4000000
+/**
+ * main - Starting point, finds and prints the sum of the even-valued terms,
+ *        followed by a new line.
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	long fn_1 = 1, fn_2 = 2, fn, sum = 2;
+	int pos = 0;
+
+	while (1)
+	{
+		fn = fn_1 + fn_2;
+		if (fn % 2 == 0 && fn <= THRESHOLD)
+			sum += fn;
+		fn_1 = fn_2;
+		fn_2 = fn;
+		if (fn > THRESHOLD)
+			break;
+	}
+	printf("%li", sum);
+	putchar('\n');
+	return (0);
+}
