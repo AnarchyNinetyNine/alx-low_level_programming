46394b864f3a6a59e9242c9ae334205c57b92b8b
Idris <franckenstein999@gmail.com>
2024-01-17 14:41:45 -0800

    Betty style fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
- /**
-  * free_grid - Frees a previously created 2 dimensional grid.
-  * @grid: A 2D grid.
-  * @height: The number of rows.
-  */
+/**
+ * free_grid - Frees the memory allocated for a 2-dimensional grid.
+ * @grid: A pointer to the 2D grid to be freed.
+ * @height: The number of rows in the gr
+ */
 
 void free_grid(int **grid, int height)
 {
