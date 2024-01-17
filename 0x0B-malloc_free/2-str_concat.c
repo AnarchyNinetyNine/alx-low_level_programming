bb43069f518ed38b149fc3d3bd60130f0e842165
Idris <franckenstein999@gmail.com>
2024-01-16 16:53:02 -0800

    Hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,9 +18,9 @@ char *str_concat(char *s1, char *s2)
 
 	size1 = size2 = i = 0;
 	if (s1 == NULL)
-		*s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		*s2 = '\0';
+		s2 = "\0";
 	while (s1[size1] != '\0')
 		++size1;
 	while (s2[size2] != '\0')
