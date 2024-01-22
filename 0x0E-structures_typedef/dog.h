2571d9ff689db9f5513fbd73a325efa75b1d9745
Idris <franckenstein999@gmail.com>
2024-01-22 15:36:53 -0800

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,12 @@
 #ifndef _DOG_H_
-#define _DiOG_H_
+#define _DOG_H_
 /**
  * struct dog - A dog poppy structure
  * @name: The dog structure's name
  * @age: The dog structure's age.
  * @owner: The dog structure's owner.
  *
- * Description: A Poopy dog structure that has a name, an
- *              age, and an owner.
+ * Description: A Poopy dog structure that has a name, an age, and an owner.
  */
 
 struct dog
