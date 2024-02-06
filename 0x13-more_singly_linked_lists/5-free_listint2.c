35796ff4a125cf05f5f5401c28289ec0d3846761
Idris <franckenstein999@gmail.com>
2024-02-06 14:51:09 -0800

    add 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - A function that frees a listint_t list,
+ *                 then, sets the head to NULL.
+ * @head: The address of the head of a listint_t linked list.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *explorer = *head;
+	listint_t *nextNode = NULL;
+
+	if (!explorer)
+		return;
+	while (explorer->next)
+	{
+		nextNode = explorer->next;
+		free(explorer);
+		explorer = nextNode;
+	}
+	free(explorer);
+	(*head) = NULL;
+}
