9f23c816ee888573e0a29867844264502b312db2
Idris <franckenstein999@gmail.com>
2024-02-06 16:53:06 -0800

    adding 9-insert_nodeint.c and lists.h

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *previous, *node = *head;
 	listint_t *new = malloc(sizeof(listint_t));
 
-	if (!head || *head == NULL)
+	if (!head || *head == NULL || !new)
 		return (NULL);
 	while (node && count++ != idx)
 	{
