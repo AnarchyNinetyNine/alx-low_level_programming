b4fd1b559e53b5e783a53cdc88de97ed5e142b7d
Idris <franckenstein999@gmail.com>
2024-02-06 17:03:56 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		new->next = *head;
 		*head = node;
-		return(new);
+		return (new);
 	}
 	while (node && count++ != idx)
 	{
