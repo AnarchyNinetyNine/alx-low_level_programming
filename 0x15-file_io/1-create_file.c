38f723991cc1b05f56ec880822ee6f166a251dde
Idris <franckenstein999@gmail.com>
2024-02-12 18:01:53 -0800

    Hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,20 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: A pointer to a string.
+ *
+ * Return: The length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 +  _strlen_recursion(s + 1));
+}
+
+/**
  * create_file - A function that creates a file.
  * @filename: The name of the file to create.
  * @text_content: A NULL terminated string to write to the file.
@@ -11,7 +25,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = strlen(text_content);
+	ssize_t bytes = 0, len = _strlen_recursion(text_content);
 
 	if (!filename)
 		return (-1);
