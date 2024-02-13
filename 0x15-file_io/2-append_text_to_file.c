0ce9c9eada4cb203a4aa334f3e8c80c343635592
Idris <franckenstein999@gmail.com>
2024-02-12 18:14:05 -0800

    2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns the length of a string.
+ * @s: A pointer to a string.
+ *
+ * Return: The length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 +  _strlen_recursion(s + 1));
+}
+
+/**
+ * append_text_to_file - A function that appends text at the end of a file.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL terminated string to write to the file.
+ *
+ * Return: 1 on Suceess. -1 otherwise.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen_recursion(text_content);
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (len)
+		bytes = write(fd, text_content, len);
+
+	close(fd);
+	return (bytes == len ? 1 : -1);
+}
