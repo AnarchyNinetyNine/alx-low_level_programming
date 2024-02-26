deeecbc71f3f60d0c4d2c4044a6854defc6ca126
Idris <franckenstein999@gmail.com>
2024-02-26 12:37:32 -0800

    Bug fixed: handling null content

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,18 +25,20 @@ int _strlen_recursion(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes = 0, len = _strlen_recursion(text_content);
+	ssize_t bytes = 0, len = text_content != NULL ? _strlen_recursion(text_content) : 0;
 
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (len > 0)
+	if (len)
 		bytes = write(fd, text_content, len);
+	else
+		bytes = len;
 
 	close(fd);
 	return (bytes == len ? 1 : -1);
