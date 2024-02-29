dba16eaa12b0fe52d7db96d7254ec5194476c3b4
Idris <franckenstein999@gmail.com>
2024-02-29 09:05:02 -0800

    Adding more readable version of 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,6 +29,50 @@ char *tokenizer(char **placeholder)
 }
 
 /**
+ * fill_array - Filles and array wit splitted words.
+ *@arr: An array.
+ *@placeholder: The string to be splitted.
+ *@wc: WordCount.
+ *
+ *Return: The filled array.
+ */
+char **fill_array(char **arr, char *placeholder, int wc)
+{
+	int i = 0;
+
+	while (wc--)
+	{
+		arr[i] = tokenizer(&placeholder);
+		i++;
+	}
+	arr[i] = (NULL);
+	return (arr);
+}
+
+int wc(char *placeholder)
+{
+	int wc = 0;       /* wordCount */
+	int inWord = 0;   /* inside a word */
+	int i = 0;        /* iterator */
+
+	while (placeholder[i])
+	{
+		if (placeholder[i] != ' ' && inWord == 0)
+		{
+			inWord = 1; /* Open inWord mode */
+			++wc; /* Increment the wordCount */
+		}
+		else if (placeholder[i] == ' ' && inWord == 1)
+		{
+			inWord = 0; /* Clode inWord mode */
+			placeholder[i] = '\0'; /* Poison the string */
+		}
+		++i;
+	}
+	return (wc);
+}
+
+/**
  * strtow - A function that splits a string into words.
  * @str: A given string.
  *
@@ -38,40 +82,21 @@ char *tokenizer(char **placeholder)
 char **strtow(char *str)
 {
 	char *placeholder = strdup(str);
-	int wc = 0;       /* wordCount */
-	int inWord = 0;   /* inside a word */
-	int i = 0;        /* iterator */
-	char **arr;
+	int wCount = 0; /* WourdCount */
+	char **arr;     /* The malloc'ed array */
 
 	if (!str || strcmp(str, "") == 0)
-		return (NULL);
+		return (NULL); /* Exit if no string provided */
 
-	while (placeholder[i])
-	{
-		if (placeholder[i] != ' ' && inWord == 0)
-		{
-			inWord = 1;
-			++wc;
-		}
-		else if (placeholder[i] == ' ' && inWord == 1)
-		{
-			inWord = 0;
-			placeholder[i] = '\0';
-		}
-		++i;
-	}
-	if (wc == 0)
-		return (NULL);
+	wCount = wc(placeholder); /* Get the wordCount */
 
-	arr = malloc(sizeof(char *) * (wc + 1));
+	if (wCount == 0)
+		return (NULL); /* Exit if no word found */
+
+	arr = malloc(sizeof(char *) * (wCount + 1)); /* Allocate memory */
+
 	if (!arr)
-		return (NULL);
-	i = 0;
-	while (wc--)
-	{
-		arr[i] = tokenizer(&placeholder);
-		i++;
-	}
-	arr[i] = (NULL);
-	return (arr);
+		return (NULL); /* Exit if cannot allocate memory */
+
+	return (fill_array(arr, placeholder, wCount)); /* Return an rray of words */
 }
