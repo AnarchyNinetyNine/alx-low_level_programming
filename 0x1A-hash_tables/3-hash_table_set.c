c610d54d33dc2955bfbf09dee4aecb6f94f81998
Idris <franckenstein999@gmail.com>
2024-04-12 16:56:14 -0700

    fix return value in 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,12 +13,30 @@
 hash_node_t *add_node(hash_node_t **head, char *key, const char *value)
 {
 	hash_node_t *newNode = malloc(sizeof(hash_node_t));
-
+	hash_node_t *nodeHead = NULL;
 	if (!newNode)
 		return (NULL);
 
-	newNode->key = key;
+	nodeHead = *head;
+
+	while(nodeHead)
+	{
+		if (strcmp(nodeHead->key, key) == 0)
+		{
+			free(nodeHead->value);
+			nodeHead->value = strdup(value);
+			if (!nodeHead->value)
+				return (NULL);
+			return (nodeHead);
+		}
+		nodeHead = nodeHead->next;
+	}
+	newNode->key = strdup(key);
 	newNode->value = strdup(value);
+
+	if (!newNode->key || newNode->value)
+		return (NULL);
+
 	newNode->next = *head;
 	*head = newNode;
 	return (newNode);
@@ -38,13 +56,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node = NULL;
+	hash_node_t *new_node = NULL;
 
 	if (!ht || !key || !value || !ht->array || strcmp(key, "") == 0)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
-	add_node(&node, (char *)key, value);
+	new_node = add_node(&node, (char *)key, value);
 
-	return (index);
+	return (new_node != NULL ? 1 : 0);
 }
