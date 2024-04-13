fe8b7b743022f3ce4c8ec83119fc0f036cf4469f
Idris <franckenstein999@gmail.com>
2024-04-12 19:51:20 -0700

    add 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieve a value associated with a key.
+ * @ht: Hash table.
+ * @key: The key.
+ *
+ * Return: # The associated value with the element.
+ *         # NULL if key couldn't be found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (!ht || !key || strcmp(key, "") == 0)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
+
